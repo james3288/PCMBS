2abe25603fc676c7b01f8273e8a6b9292158ef3a
ellagarcia3288 <ellagarcia3288@gmail.com>
2024-08-10 17:12:19 +0800

    Add commit

---
+++ PCMBS/FMain.Designer.vb
@@ -0,0 +1,332 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FMain))
+        Panel1 = New Panel()
+        Panel6 = New Panel()
+        PictureBox3 = New PictureBox()
+        LinkLabel3 = New LinkLabel()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        LinkLabel2 = New LinkLabel()
+        LinkLabel1 = New LinkLabel()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        ToolStrip1 = New ToolStrip()
+        ToolStripButton1 = New ToolStripButton()
+        ToolStripButton2 = New ToolStripButton()
+        ToolStripButton4 = New ToolStripButton()
+        ToolStripButton3 = New ToolStripButton()
+        Panel4 = New Panel()
+        Label1 = New Label()
+        LinkLabel5 = New LinkLabel()
+        Panel1.SuspendLayout()
+        Panel6.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        Panel3.SuspendLayout()
+        ToolStrip1.SuspendLayout()
+        Panel4.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Transparent
+        Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), Image)
+        Panel1.Controls.Add(Panel6)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1266, 81)
+        Panel1.TabIndex = 0
+        ' 
+        ' Panel6
+        ' 
+        Panel6.Controls.Add(PictureBox3)
+        Panel6.Controls.Add(LinkLabel3)
+        Panel6.Controls.Add(PictureBox1)
+        Panel6.Controls.Add(PictureBox2)
+        Panel6.Controls.Add(LinkLabel2)
+        Panel6.Controls.Add(LinkLabel1)
+        Panel6.Dock = DockStyle.Top
+        Panel6.Location = New Point(0, 0)
+        Panel6.Name = "Panel6"
+        Panel6.Size = New Size(1266, 37)
+        Panel6.TabIndex = 0
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackgroundImageLayout = ImageLayout.None
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(320, 6)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(27, 27)
+        PictureBox3.TabIndex = 6
+        PictureBox3.TabStop = False
+        ' 
+        ' LinkLabel3
+        ' 
+        LinkLabel3.ActiveLinkColor = Color.DeepSkyBlue
+        LinkLabel3.AutoSize = True
+        LinkLabel3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel3.ForeColor = SystemColors.ButtonFace
+        LinkLabel3.LinkBehavior = LinkBehavior.HoverUnderline
+        LinkLabel3.LinkColor = Color.White
+        LinkLabel3.Location = New Point(353, 9)
+        LinkLabel3.Name = "LinkLabel3"
+        LinkLabel3.Size = New Size(103, 21)
+        LinkLabel3.TabIndex = 5
+        LinkLabel3.TabStop = True
+        LinkLabel3.Text = "Registration"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImageLayout = ImageLayout.None
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(31, 6)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(27, 27)
+        PictureBox1.TabIndex = 4
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackgroundImageLayout = ImageLayout.None
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(145, 6)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(27, 27)
+        PictureBox2.TabIndex = 3
+        PictureBox2.TabStop = False
+        ' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.ActiveLinkColor = Color.DeepSkyBlue
+        LinkLabel2.AutoSize = True
+        LinkLabel2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel2.ForeColor = SystemColors.ButtonFace
+        LinkLabel2.LinkBehavior = LinkBehavior.HoverUnderline
+        LinkLabel2.LinkColor = Color.White
+        LinkLabel2.Location = New Point(178, 9)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(110, 21)
+        LinkLabel2.TabIndex = 2
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "Maintenance"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.ActiveLinkColor = Color.DeepSkyBlue
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel1.ForeColor = SystemColors.ButtonFace
+        LinkLabel1.LinkBehavior = LinkBehavior.HoverUnderline
+        LinkLabel1.LinkColor = Color.White
+        LinkLabel1.Location = New Point(58, 9)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(56, 21)
+        LinkLabel1.TabIndex = 0
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Home"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.Transparent
+        Panel2.BackgroundImage = CType(resources.GetObject("Panel2.BackgroundImage"), Image)
+        Panel2.Dock = DockStyle.Bottom
+        Panel2.Location = New Point(0, 633)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(1266, 84)
+        Panel2.TabIndex = 1
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = SystemColors.GrayText
+        Panel3.BackgroundImage = CType(resources.GetObject("Panel3.BackgroundImage"), Image)
+        Panel3.Controls.Add(ToolStrip1)
+        Panel3.Dock = DockStyle.Left
+        Panel3.Location = New Point(0, 81)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(335, 552)
+        Panel3.TabIndex = 2
+        ' 
+        ' ToolStrip1
+        ' 
+        ToolStrip1.BackColor = Color.Transparent
+        ToolStrip1.Dock = DockStyle.Fill
+        ToolStrip1.GripStyle = ToolStripGripStyle.Hidden
+        ToolStrip1.Items.AddRange(New ToolStripItem() {ToolStripButton1, ToolStripButton2, ToolStripButton4, ToolStripButton3})
+        ToolStrip1.LayoutStyle = ToolStripLayoutStyle.Flow
+        ToolStrip1.Location = New Point(0, 0)
+        ToolStrip1.Name = "ToolStrip1"
+        ToolStrip1.RenderMode = ToolStripRenderMode.System
+        ToolStrip1.Size = New Size(335, 552)
+        ToolStrip1.TabIndex = 1
+        ToolStrip1.Text = "ToolStrip1"
+        ' 
+        ' ToolStripButton1
+        ' 
+        ToolStripButton1.BackgroundImageLayout = ImageLayout.None
+        ToolStripButton1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ToolStripButton1.ForeColor = Color.White
+        ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), Image)
+        ToolStripButton1.ImageScaling = ToolStripItemImageScaling.None
+        ToolStripButton1.ImageTransparentColor = Color.Magenta
+        ToolStripButton1.Name = "ToolStripButton1"
+        ToolStripButton1.Padding = New Padding(10)
+        ToolStripButton1.Size = New Size(142, 139)
+        ToolStripButton1.Text = "Requisition Slip"
+        ToolStripButton1.TextImageRelation = TextImageRelation.ImageAboveText
+        ' 
+        ' ToolStripButton2
+        ' 
+        ToolStripButton2.BackgroundImageLayout = ImageLayout.None
+        ToolStripButton2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ToolStripButton2.ForeColor = Color.White
+        ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), Image)
+        ToolStripButton2.ImageScaling = ToolStripItemImageScaling.None
+        ToolStripButton2.ImageTransparentColor = Color.Magenta
+        ToolStripButton2.Name = "ToolStripButton2"
+        ToolStripButton2.Padding = New Padding(10)
+        ToolStripButton2.Size = New Size(154, 139)
+        ToolStripButton2.Text = "Quantity Take Off"
+        ToolStripButton2.TextImageRelation = TextImageRelation.ImageAboveText
+        ' 
+        ' ToolStripButton4
+        ' 
+        ToolStripButton4.BackgroundImageLayout = ImageLayout.None
+        ToolStripButton4.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ToolStripButton4.ForeColor = Color.White
+        ToolStripButton4.Image = CType(resources.GetObject("ToolStripButton4.Image"), Image)
+        ToolStripButton4.ImageScaling = ToolStripItemImageScaling.None
+        ToolStripButton4.ImageTransparentColor = Color.Magenta
+        ToolStripButton4.Name = "ToolStripButton4"
+        ToolStripButton4.Padding = New Padding(10)
+        ToolStripButton4.Size = New Size(210, 139)
+        ToolStripButton4.Text = "Project Code & Date Range"
+        ToolStripButton4.TextImageRelation = TextImageRelation.ImageAboveText
+        ' 
+        ' ToolStripButton3
+        ' 
+        ToolStripButton3.BackgroundImageLayout = ImageLayout.None
+        ToolStripButton3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ToolStripButton3.ForeColor = Color.White
+        ToolStripButton3.Image = CType(resources.GetObject("ToolStripButton3.Image"), Image)
+        ToolStripButton3.ImageScaling = ToolStripItemImageScaling.None
+        ToolStripButton3.ImageTransparentColor = Color.Magenta
+        ToolStripButton3.Name = "ToolStripButton3"
+        ToolStripButton3.Padding = New Padding(10)
+        ToolStripButton3.Size = New Size(118, 139)
+        ToolStripButton3.Text = "Reports"
+        ToolStripButton3.TextImageRelation = TextImageRelation.ImageAboveText
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackColor = Color.Transparent
+        Panel4.Controls.Add(Label1)
+        Panel4.Controls.Add(LinkLabel5)
+        Panel4.Dock = DockStyle.Fill
+        Panel4.Location = New Point(335, 81)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(931, 552)
+        Panel4.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(18, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(202, 30)
+        Label1.TabIndex = 5
+        Label1.Text = "Welcome to PCMBS,"
+        ' 
+        ' LinkLabel5
+        ' 
+        LinkLabel5.ActiveLinkColor = Color.DeepSkyBlue
+        LinkLabel5.AutoSize = True
+        LinkLabel5.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel5.ForeColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        LinkLabel5.LinkBehavior = LinkBehavior.HoverUnderline
+        LinkLabel5.LinkColor = Color.FromArgb(CByte(255), CByte(128), CByte(0))
+        LinkLabel5.Location = New Point(218, 21)
+        LinkLabel5.Name = "LinkLabel5"
+        LinkLabel5.Size = New Size(123, 32)
+        LinkLabel5.TabIndex = 4
+        LinkLabel5.TabStop = True
+        LinkLabel5.Text = "King3288"
+        ' 
+        ' FMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Window
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(1266, 717)
+        Controls.Add(Panel4)
+        Controls.Add(Panel3)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        Name = "FMain"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Main Menu"
+        WindowState = FormWindowState.Maximized
+        Panel1.ResumeLayout(False)
+        Panel6.ResumeLayout(False)
+        Panel6.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        ToolStrip1.ResumeLayout(False)
+        ToolStrip1.PerformLayout()
+        Panel4.ResumeLayout(False)
+        Panel4.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripButton4 As ToolStripButton
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents LinkLabel5 As LinkLabel
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ PCMBS/FMain.vb
@@ -0,0 +1,9 @@
+﻿Public Class FMain
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+
+    End Sub
+End Class
